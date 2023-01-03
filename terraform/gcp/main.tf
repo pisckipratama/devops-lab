1db462cea45993fe5d22d33f6acdebecafc91e06
Piscki Pratama <pisckipratama@gmail.com>
2023-01-03 10:45:53 +0700

    learning list type data

--- terraform/gcp/main.tf
+++ terraform/gcp/main.tf
@@ -4,7 +4,8 @@ provider "google" {
 }
 
 variable "subnet_ip_cidr_range" {
-  description = "subnet for dev environment"
+  description = "ip range untuk semua subnet"
+  type        = list(string)
 }
 
 variable "subnet_secondary_ip_cidr_range" {
@@ -15,8 +16,9 @@ variable "network_name" {
   description = "nama network kita"
 }
 
-variable "subnet_01_name" {
+variable "subnet_name_list" {
   description = "nama subnet 01"
+  type        = list(string)
 }
 
 resource "google_compute_network" "development_network" {
@@ -25,8 +27,8 @@ resource "google_compute_network" "development_network" {
 }
 
 resource "google_compute_subnetwork" "dev_subnet_01" {
-  name          = var.subnet_01_name
-  ip_cidr_range = var.subnet_ip_cidr_range
+  name          = var.subnet_name_list[0]
+  ip_cidr_range = var.subnet_ip_cidr_range[0]
   region        = "asia-southeast2"
   network       = google_compute_network.development_network.id
   secondary_ip_range {
@@ -35,12 +37,19 @@ resource "google_compute_subnetwork" "dev_subnet_01" {
   }
 }
 
+resource "google_compute_subnetwork" "dev_subnet_02" {
+  name          = var.subnet_name_list[1]
+  ip_cidr_range = var.subnet_ip_cidr_range[1]
+  region        = "asia-southeast1"
+  network       = google_compute_network.development_network.id
+}
+
 data "google_compute_network" "default_network_existing" {
   name = "default"
 }
 
-resource "google_compute_subnetwork" "dev_subnet_02" {
-  name          = "dev-subnet-02"
+resource "google_compute_subnetwork" "testing-subnet" {
+  name          = "testing-subnet"
   ip_cidr_range = "10.110.0.0/16"
   network       = data.google_compute_network.default_network_existing.id
   region        = "asia-southeast2"
