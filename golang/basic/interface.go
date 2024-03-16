4463a4125a3639cc2b67d400300b224b9497f21e
Piscki Pratama <pisckipratama@gmail.com>
2024-03-17 00:23:45 +0700

    edit interface file

--- golang/basic/interface.go
+++ golang/basic/interface.go
@@ -1,7 +1,41 @@
 package main
 
+import "fmt"
+
 /**
 - interface adalah tipe data Abstract, dia tidak memiliki implementasi langsung
 - sebuah interface berisikan definisi-definisi method
 - biasanya interface digunakan sebagai kontrak
 */
+
+type HasName interface {
+	GetName() string
+}
+
+func SayHello(value HasName) {
+	fmt.Println("Hello", value.GetName())
+}
+
+type Person struct {
+	Name string
+}
+
+type Animal struct {
+	Name string
+}
+
+func (person Person) GetName() string {
+	return person.Name
+}
+
+func (animal Animal) GetName() string {
+	return animal.Name
+}
+
+func main() {
+	person := Person{Name: "Piscki"}
+	SayHello(person)
+
+	animal := Animal{Name: "Kuda"}
+	SayHello(animal)
+}
