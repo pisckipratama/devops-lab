f1fa379f6f1dfb1c501b7e3085c6407b9e81e41f
Piscki Pratama <pisckipratama@gmail.com>
2024-03-24 16:21:06 +0700

    add reflect package part 2

--- golang/package/reflect.go
+++ golang/package/reflect.go
@@ -6,12 +6,14 @@ import (
 )
 
 type Sample struct {
-	Name string
+	Name string `required:"true" max:"10"`
 }
 
 type Person struct {
-	Name, Email, Address string
-	Age                  int
+	Name    string `required:"true" max:"10"`
+	Email   string `required:"true" max:"10"`
+	Address string `required:"true" max:"10"`
+	Age     int
 }
 
 func readField(value any) {
@@ -19,12 +21,39 @@ func readField(value any) {
 	fmt.Println("Type name", valueType.Name())
 
 	for i := 0; i < valueType.NumField(); i++ {
-		var valueField reflect.StructField = valueType.Field(i)
-		fmt.Println(valueField.Name, "with type", valueField.Type)
+		var structField reflect.StructField = valueType.Field(i)
+		fmt.Println(structField.Name, "with type", structField.Type)
+		fmt.Println(structField.Tag.Get("required"))
+		fmt.Println(structField.Tag.Get("max"))
+		fmt.Println("===")
 	}
+}
+
+func isValid(value any) (result bool) {
+	result = true
+	t := reflect.TypeOf(value)
+	for i := 0; i < t.NumField(); i++ {
+		f := t.Field(i)
+		if f.Tag.Get("required") == "true" {
+			data := reflect.ValueOf(value).Field(i).Interface()
+			result = data != ""
+			if result == false {
+				return result
+			}
+		}
+	}
+	return result
 }
 
 func main() {
 	readField(Sample{"Piscki"})
 	readField(Person{"Tama", "", "", 20})
+
+	person := Person{
+		Name:    "ada",
+		Address: "ada",
+		Email:   "ada",
+	}
+
+	fmt.Println("Ini hasil isvalid", isValid(person))
 }
